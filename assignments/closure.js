e51142fe7db68058ecc876c13fd7f227e04a66a6
littleonetwo <littleonetwo@gmail.com>
2019-09-06 17:57:34 -0500

    initial upload of the completed assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,14 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+var name = 'Mike';
+
+function greeting(){
+
+  console.log('Hi ' + name + '!!');
+}
+
+greeting();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
